5eb21650de12706631696ab8ad629e6a04d8994e
Pau Cabaneros <pau.cabaneros@gmail.com>
2025-01-30 19:25:10 +0100

    shiwch theme to pydata sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,28 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "pydata_sphinx_theme"
+]
 
-templates_path = ['_templates']
-exclude_patterns = []
+html_theme = "pydata_sphinx_theme"
 
-
+# Optional theme customization
+html_theme_options = {
+    "show_prev_next": True,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/paucablop/chemotools.docs",  # replace with your repo
+            "icon": "fab fa-github-square",
+            "type": "fontawesome",
+        }
+    ],
+    "use_edit_page_button": False,
+    "navigation_with_keys": False,
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
