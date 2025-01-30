6ae698769b54f0a01cd557f7d40001d6f80ec1a5
Pau Cabaneros <pau.cabaneros@gmail.com>
2025-01-30 18:27:21 +0100

    re-start project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,60 +1,28 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-project = 'Chemotools'
-copyright = '2025, chemotools'
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'chemotools.docs'
+copyright = '2025, Pau Cabaneros'
 author = 'Pau Cabaneros'
-
-# The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
-# Language settings
-language = 'en'
-locale_dirs = ['locale/']
-gettext_compact = False
-gettext_uuid = True
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx_copybutton',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = 'pydata_sphinx_theme'
-
-# GitHub context and other variables
-html_context = {
-    "github_user": "paucablop",
-    "github_repo": "chemotools.docs",
-    "github_version": "main",
-    "doc_path": "docs/source",
-    # Language settings
-    "languages": [
-        ('en', 'English'),
-        ('es', 'Español'),
-        ('zh', '中文'),
-    ],
-    "language": "en",
-    "default_language": "en",
-}
-
-html_theme_options = {
-    "github_url": "https://github.com/paucablop/chemotools.docs",
-    "use_edit_page_button": True,
-    "show_toc_level": 2,
-    "navbar_end": ["theme-switcher", "navbar-icon-links"]
-}
-
-# Make sure static files are properly configured
 html_static_path = ['_static']
-html_css_files = ['custom.css']
-html_js_files = ['custom.js']
-
-templates_path = ['_templates']
